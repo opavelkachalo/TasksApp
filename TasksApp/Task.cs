2f4721283fab09d36b2d997c453cf8c5fc0876aa
opavelkachalo <opavelkachalo@gmail.com>
2020-10-21 00:17:09 +0300

    Initial commit

---
+++ TasksApp/Task.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TasksApp
+{
+    public class Task : INotifyPropertyChanged
+    {
+        private string _name;
+        private DateTime? _deadLine;
+        private bool _isSelected;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public DateTime? DeadLine
+        {
+            get => _deadLine;
+            set
+            {
+                _deadLine = value;
+                OnPropertyChanged("DeadLine");
+            }
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged("IsSelected");
+            }
+        }
+
+        public Task(string name, DateTime? deadline)
+        {
+            Name = name;
+            DeadLine = deadline;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TasksApp/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace TasksApp
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Login { get; }
+        public byte[] Password { get; set; }
+        public ObservableCollection<Task> Tasks { get; set; }
+
+
+        public User(string name, DateTime? birthDate, string login, byte[] password)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Login = login;
+            Password = password;
+            Tasks = new ObservableCollection<Task>();
+        }
+    }
+}
